71a3cba1a9efa4d498a385a2972716509475fd21
guntekhunter <guntek2000@gmail.com>
2023-07-23 21:56:18 +0800

    fead: can create a user

---
+++ src/app/api/auth/route.ts
@@ -0,0 +1,48 @@
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+
+const prisma = new PrismaClient();
+
+export async function GET(req: NextRequest, res: NextResponse) {
+  return NextResponse.json({ status: "Ok" });
+}
+export async function POST(req: NextRequest) {
+  try {
+    const reqBody = await req.json();
+    const { email, name, isAdmin, password } = reqBody;
+    const secretPassword = password;
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (user) {
+      return NextResponse.json(
+        { error: "user already exist" },
+        { status: 400 }
+      );
+    }
+
+    //hash password
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+    const newUser = await prisma.user.create({
+      data: {
+        email,
+        name,
+        isAdmin,
+        password: hashedPassword,
+      },
+    });
+    return NextResponse.json({
+      status: "Ok",
+      message: "new user added",
+      newUser,
+    });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
