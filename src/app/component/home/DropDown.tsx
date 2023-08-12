420110e08f4070050541d9982a4f804c9ad0d562
guntekhunter <guntek2000@gmail.com>
2023-08-12 18:07:49 +0800

    fix: home client interface

--- src/app/component/home/DropDown.tsx
+++ src/app/component/home/DropDown.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
 
 //@ts-ignore
 export default function DropDown({ drop, title }) {
@@ -7,8 +8,6 @@ export default function DropDown({ drop, title }) {
 
   const dropDown = () => {
     setIsDrop(!isDrop);
-    console.log("terkliknyami", { title });
-    console.log(drop);
   };
 
   const select = (item: any) => {
@@ -17,25 +16,36 @@ export default function DropDown({ drop, title }) {
   };
 
   const dropData = drop;
-  console.log(dropData);
+
   return (
     <div className="relative">
       <button
-        className="flex rounded-md transition-all duration-300"
+        className="flex rounded-md bg-gray-100 border-[1.5px] border-gray-200 w-[6rem] justify-between px-[.5rem]"
         onClick={dropDown}
       >
         <p>{selected}</p>
+        <div className="flex items-bottom pt-[.1rem]">
+          <Image
+            src="/down.png"
+            alt=""
+            width={500}
+            height={500}
+            className="w-[1.2rem]"
+          />
+        </div>
       </button>
       <div
-        className={`absolute bg-white right-0 rounded-md shadow-md top-[2.5rem] ${
+        className={`absolute bg-white right-0 rounded-md shadow-md top-[2.5rem] transition-all duration-300  ${
           isDrop ? "block" : "hidden"
         }`}
       >
-        <div className={`absolute bg-white right-0 rounded-md shadow-md z-10`}>
+        <div
+          className={`absolute bg-white right-0 rounded-md shadow-md z-10 border border-[1.5px] border-gray-200`}
+        >
           {dropData?.map((item: any, key: any) => (
             <div
               key={key}
-              className="hover:bg-gray-200 w-full px-[1rem] h-full p-[.2rem]"
+              className="hover:bg-gray-200 w-full px-[1rem] h-full p-[.2rem] flex justify-center"
               onClick={(e) => select(item)}
             >
               {item}
--- src/app/component/home/HomeClient.tsx
+++ src/app/component/home/HomeClient.tsx
@@ -33,17 +33,23 @@ export default function HomeClient() {
           <div className="flex justify-between py-[1rem] border-b-[1.5px]">
             <div>Guru Kelas</div>
             <input
+              onChange={(e) => {
+                setGuru(e.target.value);
+              }}
               value={guru}
               placeholder="Salsul Rijal S.Pt"
-              className="flex justify-center text-left w-[10rem]"
+              className="flex justify-center text-left w-[10rem] rounded-md bg-gray-100 border-[1.5px] border-gray-200 px-[.5rem]"
             />
           </div>
           <div className="flex justify-between py-[1rem] border-b-[1.5px]">
             <div>NIP</div>
             <input
               value={nip}
+              onChange={(e) => {
+                setNip(e.target.value);
+              }}
               placeholder="1829040023"
-              className="flex justify-center text-left w-[10rem]"
+              className="flex justify-center text-left w-[10rem] rounded-md bg-gray-100 border-[1.5px] border-gray-200 px-[.5rem]"
             />
           </div>
           <div className="flex justify-between py-[1rem] border-b-[1.5px]">
@@ -55,7 +61,7 @@ export default function HomeClient() {
             <DropDown title="Ganjil" drop={["Ganjil", "Genap"]} />
           </div>
           <div>
-            <button className="bg-black w-full py-[1rem] rounded-md mb-[2rem] text-white mt-[2rem]">
+            <button className="bg-black w-full py-[1rem] rounded-md mb-[2rem] text-white mt-[2rem] hover:bg-black-200">
               Simpan Perubahan
             </button>
           </div>
