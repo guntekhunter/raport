1c1e6c45df393ef5df0699e3c91bd5bde3ce0254
guntekhunter <guntek2000@gmail.com>
2023-08-13 20:44:46 +0800

    fead: change the button if input is edited

--- src/app/api/main-data/route.ts
+++ src/app/api/main-data/route.ts
@@ -5,8 +5,17 @@ const prisma = new PrismaClient();
 
 export async function GET(req: NextRequest, res: NextResponse) {
   try {
-    const data = await prisma.main_data.findMany();
-    return NextResponse.json({ status: "Ok", data });
+    const reqBody = req.nextUrl.searchParams.get("id_user");
+    // const reqBody = await req.json();
+    const id_user = reqBody;
+    console.log(id_user);
+    // const data = await prisma.main_data.findFirst({
+    //   where: {
+    //     id_user,
+    //   },
+    // });
+
+    return NextResponse.json({ status: "Ok", data: reqBody });
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 400 });
   }
@@ -17,6 +26,19 @@ export async function POST(req: NextRequest) {
     const reqBody = await req.json();
     const { guru_kelas, nip, kelas_huruf, kelas_angka, semester, id_user } =
       reqBody;
+
+    const newData = await prisma.main_data.create({
+      data: {
+        guru_kelas,
+        nip,
+        kelas_huruf,
+        kelas_angka,
+        semester,
+        id_user,
+      },
+    });
+
+    return NextResponse.json({ status: "Ok", newData });
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
--- src/app/component/home/HomeClient.tsx
+++ src/app/component/home/HomeClient.tsx
@@ -18,10 +18,12 @@ export default function HomeClient() {
   const [grade, setGrade] = useState("");
   const [semester, setSemester] = useState("");
   const [data, setData] = useState<DataItem[]>([]);
+  const [buttonActive, setButtonActive] = useState(false);
 
   const route = useRouter();
 
   const classCallback = (item: any, name: any) => {
+    setButtonActive(true);
     if (name === "kelas") {
       setGrade(item);
     }
@@ -35,8 +37,7 @@ export default function HomeClient() {
   useEffect(() => {
     const fetch = async () => {
       try {
-        const res = await axios.get("/api/main-data");
-        console.log(res);
+        const res = await axios.get("/api/main-data?id_user=1");
       } catch (error) {
         console.log(error);
       }
@@ -53,10 +54,15 @@ export default function HomeClient() {
       semester: semester,
     };
 
-    setData((prevData) => [...prevData, newData]);
+    setData([newData]);
+    setGuru("");
+    setNip("");
+    setGrade("");
+    setSemester("");
+    setButtonActive(false);
   };
 
-  console.log(data)
+  console.log(data);
 
   return (
     <div className="flex justify-around py-[2rem] bg-gray-50">
@@ -66,7 +72,7 @@ export default function HomeClient() {
             <div>Guru Kelas</div>
             <input
               onChange={(e) => {
-                setGuru(e.target.value);
+                setGuru(e.target.value), setButtonActive(true);
               }}
               value={guru}
               placeholder="Salsul Rijal S.Pt"
@@ -78,10 +84,10 @@ export default function HomeClient() {
             <input
               value={nip}
               onChange={(e) => {
-                setNip(e.target.value);
+                setNip(e.target.value), setButtonActive(true);
               }}
               placeholder="1829040023"
-              className="flex justify-center text-left w-[10rem] rounded-md bg-gray-100 border-[1.5px] border-gray-200 px-[.5rem]"
+              className="flex justify-center text-left w-[10rem] rounded-md bg-gray-100 border-[1.5px] border-gray-200"
             />
           </div>
           <div className="flex justify-between py-[1rem] border-b-[1.5px]">
@@ -104,7 +110,11 @@ export default function HomeClient() {
           </div>
           <div>
             <button
-              className="bg-black w-full py-[1rem] rounded-md mb-[2rem] text-white mt-[2rem] hover:bg-black-200"
+              className={` w-full py-[1rem] rounded-md mb-[2rem] text-white mt-[2rem] hover:bg-black-200 ${
+                buttonActive
+                  ? "bg-black"
+                  : "bg-gray-100 border-[1.5px] border-gray-300 text-gray-300"
+              }`}
               onClick={save}
             >
               Simpan Perubahan
