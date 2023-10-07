14cf0a3b7033969754201f1d8bf5605238f29a03
guntekhunter <guntek2000@gmail.com>
2023-10-07 09:25:41 +0800

    feat: edge funtion on API

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -2,7 +2,10 @@ import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
-
+export const config = {
+  runtime: "edge", // this is a pre-requisite
+  regions: ["iad1"], // only execute this function in iad1
+};
 export async function GET(req: NextRequest, res: NextResponse) {
   try {
     const user = await prisma.user.findMany();
