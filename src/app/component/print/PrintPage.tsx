72ccedeee03461bc051dad362b48d7028dea58b8
guntekhunter <guntek2000@gmail.com>
2023-10-11 09:20:48 +0800

    feat: print api

--- src/app/component/print/PrintPage.tsx
+++ src/app/component/print/PrintPage.tsx
@@ -1,14 +1,39 @@
-import React from "react";
+"use client";
+import axios from "axios";
+import React, { useEffect, useRef, useState } from "react";
+import { useReactToPrint } from "react-to-print";
 
-export default function PrintPage() {
+// @ts-ignore
+export default function PrintPage({ id }) {
+  const [student, setStudent] = useState([]);
+  const ref = useRef<HTMLDivElement>(null);
+  const print = useReactToPrint({
+    content: () => ref.current,
+  });
+
+  useEffect(() => {
+    const fetchStudent = async () => {
+      try {
+        const data = await axios.get(
+          `http://localhost:3000/api/print?id=${id}`
+        );
+        setStudent(data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchStudent();
+  }, [id]);
+
+  useEffect(() => {
+    if (id) {
+      print();
+    }
+  }, [id]);
+
+  console.log(student);
   return (
-    <div className="relative">
-      {/* <div className="w-full justify-center flex fixed">
-        <button className="bg-primary text-white px-5 py-1 rounded-md mt-3">
-          Print
-        </button>
-      </div> */}
-
+    <div className="relative" ref={ref}>
       <div className="justify-around flex">ommalekanya mi kjsda ahhay</div>
     </div>
   );
